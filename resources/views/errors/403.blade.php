dab15b8e03fad75ddc3f89666fe540203519c00e
joisv <joisvvv@hotmail.com>
2023-09-22 14:33:13 +0700

    fix: add error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+<x-home-layout>
+    <x-slot name="title">
+       <title>403 forbidden | {{ $site_name }}</title>
+    </x-slot>
+
+    <div class="min-h-[50vh] mt-20 flex justify-center items-center">
+        <h1 class="text-rose-500 text-4xl font-semibold">403 <span class="text-gray-400">forbidden</span></h1>
+    </div>
+</x-home-layout>
