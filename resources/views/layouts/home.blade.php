7c2819384ec3c78624a018ed0578f4f42624763d
joisv <joisvvv@hotmail.com>
2023-11-21 13:27:59 +0700

    responsif info

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -118,34 +118,35 @@
     
     }">
         @include('layouts.home-navigation')
-        @if ($setting->is_info_active)
-        @empty(!$setting->info)
-            <div class="text-white p-2 w-full bg-gray-800 space-y-2">
-                <div class="flex space-x-2 items-center">
-                    <svg width="24px" height="24px" viewBox="0 0 24.00 24.00" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path d="M12 17V11" stroke="#e5e7eb" stroke-width="1.5" stroke-linecap="round"></path>
-                            <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)"
-                                fill="#e5e7eb">
-                            </circle>
-                            <path
-                                d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
-                                stroke="#e5e7eb" stroke-width="1.5" stroke-linecap="round"></path>
-                        </g>
-                    </svg>
-                    <h1 class="text-gray-300 font-bold text-lg">Info</h1>
+        @if (request()->is('/'))
+            @if ($setting->is_info_active)
+            @empty(!$setting->info)
+                <div class="text-white p-2 w-full bg-gray-800 space-y-2">
+                    <div class="flex space-x-2 items-center">
+                        <svg width="24px" height="24px" viewBox="0 0 24.00 24.00" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path d="M12 17V11" stroke="#e5e7eb" stroke-width="1.5" stroke-linecap="round"></path>
+                                <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)"
+                                    fill="#e5e7eb">
+                                </circle>
+                                <path
+                                    d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
+                                    stroke="#e5e7eb" stroke-width="1.5" stroke-linecap="round"></path>
+                            </g>
+                        </svg>
+                        <h1 class="text-gray-300 font-bold text-lg">Info</h1>
+                    </div>
+                    <article
+                        class="prose prose-base lg:prose-lg prose-code:text-rose-500 prose-a:text-blue-600 prose-headings:text-gray-200 prose-blockquote:text-white">
+                        {!! $setting->info !!}
+                    </article>
                 </div>
-                <article
-                    class="prose prose-base lg:prose-lg prose-code:text-rose-500 prose-a:text-blue-600 prose-headings:text-gray-200 prose-blockquote:text-white">
-                    {!! $setting->info !!}
-                </article>
-            </div>
-        @endempty
-    @endif
-    @if (request()->is('/'))
+            @endempty
+        @endif
+
         <div
             class="md:h-[50vh] relative bg-transparent flex items-center justify-start overflow-hidden mt-[5vh] max-w-screen-2xl w-full">
             <video src="{{ $setting->banner_video_url }}" autoplay muted="muted" preload="auto" loop="loop"
